7d36c9f5416d3a2846e2927267935df2f56dfb82
mstellaluna <mstellaluna@users.noreply.github.com>
2018-04-15 18:05:03 -0400

    Change colors individually to make gradient

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
 var css = document.querySelector("h3")
-var body = document.getElementById("gradient")
 var color1 = document.querySelector(".color1")
 var color2 = document.querySelector(".color2")
+var body = document.getElementById("gradient")
+var linearDirection = document.getElementsByName("toDirection")[0]
 
 //displays default CSS RGBA values for linear-gradient
-
+ 
 function currentSettings() {
     var CSSprop = window.getComputedStyle(body,null).getPropertyValue("background-image")
    // console.log(CSSprop)
     css.textContent = CSSprop
-   
 }
+
 currentSettings()
 
-//retrievs the select dropdown value 
 
 function directionChange(select) {
     var selectedOption = select.options[select.selectedIndex]
     var selectedText = selectedOption.text
-    console.log(selectedText)
+  //  console.log(selectedText)
     return setGradient(selectedText)
 }
 
-function setGradient(gradDir){
-    body.style.background = 
+
+function returnColor(){
+  
+  body.style.background = 
     "linear-gradient("
-    + gradDir
+    + linearDirection.value
     + ", "
     + color1.value
     + ","
     + color2.value
     + ")";
 
-    css.textContent = body.style.background 
+    currentSettings()
+  
 }
 
-color1.addEventListener("input", setGradient)
-color2.addEventListener("input", setGradient)
+document.querySelector('select[name="toDirection"]').onchange=returnColor;
+color1.addEventListener("input", returnColor)
+color2.addEventListener("input", returnColor)
+
